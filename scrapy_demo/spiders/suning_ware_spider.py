c1342b7a3532b6a48b2a0fe138a80b505aae96b7
Alex <luckboy323@gmail.com>
2016-11-23 18:07:46 +0800

    新增建表的SQL语句

--- scrapy_demo/spiders/suning_ware_spider.py
+++ scrapy_demo/spiders/suning_ware_spider.py
@@ -40,28 +40,3 @@ class LianjiaSpider(scrapy.Spider):
              print 'http://m.suning.com/product/%s/%s.html' % (ware['salesCode'],ware['catentryId'])
 
 
-
-
-
-        # lis = response.xpath('//ul[contains(@id, "productsList")]/li')
-        #
-        # for li in lis:
-        #     wares = li.xpath('./a/div[2]/p[1]/text()').extract()[0]
-        #     url = li.xpath('./a/@href').extract()[0]
-        #     comments = li.xpath('./a/div[2]/div/p[2]/em[1]/i/text()').extract()
-        #     if len(comments)>0:
-        #         comment = comments[0]
-        #     goodRates = li.xpath('./a/div[2]/div/p[2]/em[2]/i/text()').extract()
-        #     if len(goodRates)>0:
-        #         goodRate = goodRates[0]
-        #     log.msg("-----wares=%s--url=%s---comment=%s---goodRate=%s" % (wares,url,comment,goodRate), level=log.DEBUG, spider=self.name)
-            # print "-----wares=%s--url=%s---comment=%s---goodRate=%s" % (wares,url,comment,goodRate)
-
-                # item = SuningItem()
-                # item['category'] = category[0]
-                # item['ware'] = wareName
-                # item['wareUrl'] = wareUrl
-                # item['imgUrl'] = imgUrl
-                # item['memo'] = ''
-                # yield item
-
