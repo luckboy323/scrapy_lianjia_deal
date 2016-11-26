df038be83795caf09e9114ec5227dd3b6c39aa05
luckboy323 <luckboy323@gmail.com>
2016-11-26 18:54:32 +0800

    检测git的pycharm配置

--- Begin.py
+++ Begin.py
@@ -3,5 +3,4 @@ from scrapy import cmdline
 #cmdline.execute("scrapy crawl scrapy_demo".split())
 
 cmdline.execute("scrapy crawl jd_ware_spider".split())
-
 # cmdline.execute("scrapy crawl jd_category_spider".split())
